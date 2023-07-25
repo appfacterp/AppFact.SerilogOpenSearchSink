d8b9e65c1038f5f582b1baa9fe6d459d1dce17b9
kirill <polferov@live.com>
2023-07-25 15:53:42 +0200

    more extension methods

--- AppFact.SerilogOpenSearchSink/SerilogExtensions.cs
+++ AppFact.SerilogOpenSearchSink/SerilogExtensions.cs
@@ -12,4 +12,28 @@ public static class SerilogExtensions
     {
         return configuration.Sink(new OpenSearchSink(settings, options));
     }
+
+    public static LoggerConfiguration OpenSearch(this LoggerSinkConfiguration configuration,
+        string uri,
+        string basicAuthUser, string basicAuthPassword, string index = "logs", int? maxBatchSize = 1000,
+        double tickInSeconds = 1.0)
+    {
+        _ = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        _ = uri ?? throw new ArgumentNullException(nameof(uri));
+        _ = basicAuthUser ?? throw new ArgumentNullException(nameof(basicAuthUser));
+        _ = basicAuthPassword ?? throw new ArgumentNullException(nameof(basicAuthPassword));
+        _ = index ?? throw new ArgumentNullException(nameof(index));
+
+        var conn = new ConnectionSettings(new Uri(uri));
+        conn.BasicAuthentication(basicAuthUser, basicAuthPassword);
+        conn.DefaultIndex(index);
+
+        var opts = new OpenSearchSinkOptions()
+        {
+            MaxBatchSize = maxBatchSize,
+            Tick = TimeSpan.FromSeconds(tickInSeconds),
+        };
+
+        return configuration.OpenSearch(conn, opts);
+    }
 }
